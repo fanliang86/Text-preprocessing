23fd5ba72195b4418d8ecd3de838904d3f7aeea2
Fan Liang <43138926+fanliang86@users.noreply.github.com>
2019-02-20 16:40:44 -0500

    Update text_preprocessing.R

--- text_preprocessing.R
+++ text_preprocessing.R
@@ -1,24 +1,24 @@
 ### Text preprocess in R ###
 
-
 ################################################################
 ### Install packages
 rm(list=ls()) #clearing workspace
 library(tm) #package for text mining
 library(stringr)
 
-
 ################################################################
 ## Load the dataset
-### This example will use 100 Facebook posts as example for text preprocessing. The dataset has two column: one is Facebok posts (Message), while another is content topic. 
+### This example will use 100 Facebook posts as example for text preprocessing. 
+### The dataset has two column: one is Facebok posts (Message), while another is content topic. 
 data <- read.csv(file="Github/Text preprocessing_example.csv", header=T)
-data$Message[1:3] #check the first three posts
-
+data$Message[1:3] #check the posts. 
+### We can see these posts often contain lots of words and other characters like urls. Before we conduct content analysis, we need to simplify these texts.
 
 # Next, we need to transform the text file into a corpus for preprocessing
-corpus <- Corpus(VectorSource(data$Message))
+corpus <- Corpus(VectorSource(data$Message)) #make sure your put the text feature in 'Corpus' function (use 'data$Message' instead of 'data')
 
-# The main idea of text preprocessing is to simplify our textual for further analysis. In principle, there have several steps for preprocessing including remove stopwords, lowercasing, remove numbers and Punctuation. It is important to remember that the order and choice of these steps will influence your final textual datase. See: Denny, M. J., & Spirling, A. (2018). Text preprocessing for unsupervised learning: Why it matters, when it misleads, and what to do about it. Political Analysis, 26(2), 168-189.
+# The main idea of text preprocessing is to simplify our textual for further analysis. In principle, there have several steps for preprocessing including remove stopwords, lowercasing, remove numbers and Punctuation. 
+# It is important to remember that the order and choice of these steps will influence your final textual datase. See: Denny, M. J., & Spirling, A. (2018). Text preprocessing for unsupervised learning: Why it matters, when it misleads, and what to do about it. Political Analysis, 26(2), 168-189.
 
 # These are common steps:
 corpus_clean <- tm_map(corpus, tolower) #convert to lowercase 
@@ -46,8 +46,10 @@ Preprocess_posts <- function(X) {
   gsub(" ?(f|ht)tp(s?)://(.*)[.][a-z]+", "", X)
 }
 
-corpus_clean <- tm_map(corpus_clean, Preprocess_posts) 
+corpus_clean <- tm_map(corpus_clean, Preprocess_posts) #apply the function to our corpus
 
-#Finally let's check the result 
+#Finally let's check the result by comparing the original texts with the preprocess texts
 data$Message[[5]]
 as.character(corpus_clean[[5]])
+
+# If the results are not good, we can change the steps of preprocessing or add more steps. 
